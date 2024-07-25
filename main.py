0b42f121a8b1a3e3abeff4e1789fefeae199524e
Alen Subasic <alensubasic@gmail.com>
2024-07-25 10:45:47 -0500

    removing index.html generator

--- main.py
+++ main.py
@@ -76,42 +76,42 @@ def generate_m3u8_file(acestream_data, filename, server_ip):
                 f.write(f"#EXTINF:-1,{text_content}\n")
                 f.write(f"{base_url}{acestream_id}\n")
 
-def generate_index_html(channels):
-    html_content = f"""<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>HLS Video Player</title>
-<link href="https://vjs.zencdn.net/7.14.3/video-js.css" rel="stylesheet">
-<script src="https://vjs.zencdn.net/7.14.3/video.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/videojs-contrib-hls/5.15.0/videojs-contrib-hls.min.js"></script>
-</head>
-<body>
-<select id="channel-select">
-    {"".join([f'<option value="{channel}">{channel}</option>' for channel in channels])}
-</select>
+# def generate_index_html(channels):
+#     html_content = f"""<!DOCTYPE html>
+# <html lang="en">
+# <head>
+# <meta charset="UTF-8">
+# <meta name="viewport" content="width=device-width, initial-scale=1.0">
+# <title>HLS Video Player</title>
+# <link href="https://vjs.zencdn.net/7.14.3/video-js.css" rel="stylesheet">
+# <script src="https://vjs.zencdn.net/7.14.3/video.min.js"></script>
+# <script src="https://cdnjs.cloudflare.com/ajax/libs/videojs-contrib-hls/5.15.0/videojs-contrib-hls.min.js"></script>
+# </head>
+# <body>
+# <select id="channel-select">
+#     {"".join([f'<option value="{channel}">{channel}</option>' for channel in channels])}
+# </select>
 
-<video id="hls-video" class="video-js vjs-default-skin" controls preload="auto" width="640" height="360">
-    <source id="video-source" src="{channels[0]}.m3u8" type="application/x-mpegURL">
-</video>
+# <video id="hls-video" class="video-js vjs-default-skin" controls preload="auto" width="640" height="360">
+#     <source id="video-source" src="{channels[0]}.m3u8" type="application/x-mpegURL">
+# </video>
 
-<script>
-    var player = videojs('hls-video');
-    var select = document.getElementById('channel-select');
-    var source = document.getElementById('video-source');
+# <script>
+#     var player = videojs('hls-video');
+#     var select = document.getElementById('channel-select');
+#     var source = document.getElementById('video-source');
 
-    select.addEventListener('change', function() {
-        var selectedChannel = select.value;
-        source.src = selectedChannel + ".m3u8";
-        player.load();
-    });
-</script>
-</body>
-</html>
-"""
-    with open("index.html", "w") as f:
-        f.write(html_content)
+#     select.addEventListener('change', function() {
+#         var selectedChannel = select.value;
+#         source.src = selectedChannel + ".m3u8";
+#         player.load();
+#     });
+# </script>
+# </body>
+# </html>
+# """
+#     with open("index.html", "w") as f:
+#         f.write(html_content)
     
 def main():
     logging.info("\n" + "*" * 50)
@@ -138,8 +138,8 @@ def main():
                 channels.append(query.strip('[]'))
                 logging.info(f"\nGenerated {playlist_filename} with {len(acestream_data)} entries.")
         
-        generate_index_html(channels)
-        logging.info("Generated index.html for web player.\n")
+        # generate_index_html(channels)
+        # logging.info("Generated index.html for web player.\n")
         
         logging.info(f"Waiting for {update_interval / 3600} hours before next update...\n")
         time.sleep(update_interval)
